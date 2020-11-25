84f1d83cc34251416c0e1b1bbbcdfcd8b7c083b4
jose reis <jvpreis@gmail.com>
2020-11-25 20:14:26 +0000

    update countA

--- Git-Maven-Jenkins/src/test/java/Exemplo_ESII/Git_Maven_Jenkins/AppTest.java
+++ Git-Maven-Jenkins/src/test/java/Exemplo_ESII/Git_Maven_Jenkins/AppTest.java
@@ -69,7 +69,7 @@ public class AppTest extends TestCase{
 	@Test
 	void testCountA() {
 		App test=new App();
-		int output=test.countA("até funcionou. A");
+		int output=test.countA("até funcionou. Aaaaa");
 		assertEquals(2, output);
 	}
     
