20de84b3c7592f19cd6d34183294da555636687f
jose reis <jvpreis@gmail.com>
2020-11-26 15:32:17 +0000

    update count 1000

--- Git-Maven-Jenkins/src/test/java/Exemplo_ESII/Git_Maven_Jenkins/AppTest.java
+++ Git-Maven-Jenkins/src/test/java/Exemplo_ESII/Git_Maven_Jenkins/AppTest.java
@@ -69,7 +69,7 @@ public class AppTest extends TestCase{
 	@Test
 	void testCountA() {
 		App test=new App(); 
-		int output=test.countA("até funcionou.");
+		int output=test.countA("até funcionou. aaaa");
 		assertEquals(1, output);
 	}
     
