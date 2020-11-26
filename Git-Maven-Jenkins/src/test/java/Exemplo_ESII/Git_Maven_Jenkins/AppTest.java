afb4503a70380b4c3c292252c6dae2dfbc8737fb
jose reis <jvpreis@gmail.com>
2020-11-26 15:47:19 +0000

    ttt

--- Git-Maven-Jenkins/src/test/java/Exemplo_ESII/Git_Maven_Jenkins/AppTest.java
+++ Git-Maven-Jenkins/src/test/java/Exemplo_ESII/Git_Maven_Jenkins/AppTest.java
@@ -69,7 +69,7 @@ public class AppTest extends TestCase{
 	@Test
 	void testCountA() {
 		App test=new App(); 
-		int output=test.countA("até funcionou. aaaa");
+		int output=test.countA("até funcionou. aa");
 		assertEquals(1, output);
 	}
     
